e3c2f9fb220bc18c8b09804a166afe751afadcf8
alfiooatmaja <alfiooatmaja@gmail.com>
2023-07-10 16:47:04 +0700

    first

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = '643a3070e74444e9794f';
+  static const String projectId = '642ceceb8328ebfbed99';
+  static const String endPoint = 'https://baas.pasarjepara.com/v1';
+}
