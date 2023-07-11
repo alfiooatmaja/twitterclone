14c14bc24f84d32f3d3ce3540d7e3998e586065f
alfiooatmaja <alfiooatmaja@gmail.com>
2023-07-11 23:27:00 +0700

    picking multiple images dan displaying in carousel

--- lib/core/utils.dart
+++ lib/core/utils.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void showSnackbar(BuildContext context, String message) {
   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
@@ -10,4 +13,16 @@ String getNameFrom(String email) {
   //alfiooatmaja@gmail.com
   //List = [alfiooatmaja@gmail.com]
   return email.split('@')[0];
+}
+
+Future<List<File>> pickImages() async {
+  List<File> images = [];
+  final ImagePicker picker = ImagePicker();
+  final imageFiles = await picker.pickMultiImage();
+  if (imageFiles.isNotEmpty) {
+    for (final image in imageFiles) {
+      images.add(File(image.path));
+    }
+  }
+  return images;
 }
--- lib/features/tweet/views/create_tweet_view.dart
+++ lib/features/tweet/views/create_tweet_view.dart
@@ -1,8 +1,12 @@
+import 'dart:io';
+
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:twitter_clone/common/common.dart';
 import 'package:twitter_clone/constants/assets_constants.dart';
+import 'package:twitter_clone/core/core.dart';
 import 'package:twitter_clone/features/auth/controller/auth_controller.dart';
 import 'package:twitter_clone/theme/pallete.dart';
 
@@ -18,12 +22,19 @@ class CreateTweetScreen extends ConsumerStatefulWidget {
 
 class _CreateTweetScreenState extends ConsumerState<CreateTweetScreen> {
   final tweetTextController = TextEditingController();
+  List<File> image = [];
+
   @override
   void dispose() {
     super.dispose();
     tweetTextController.dispose();
   }
 
+  void onPickImage() async {
+    image = await pickImages();
+    setState(() {});
+  }
+  
   @override
   Widget build(BuildContext context) {
     final currentUser = ref.watch(currentUserDetailsProvider).value;
@@ -74,9 +85,27 @@ class _CreateTweetScreenState extends ConsumerState<CreateTweetScreen> {
                             ),
                             maxLines: null,
                           ),
-                        )
+                        ),
                       ],
                     ),
+                     if (images.isNotEmpty)
+                      CarouselSlider(
+                        items: images.map(
+                          (file) {
+                            return Container(
+                              width: MediaQuery.of(context).size.width, 
+                              margin: const EdgeInsets.symmetric(
+                                horizontal: 5,
+                              ),
+                              child: Image.file(file)
+                            );
+                          },
+                        ).toList(),
+                        options: CarouselOptions(
+                          height: 400,
+                          enableInfiniteScroll: false,
+                        ),
+                      ),
                   ],
                 ),
               ),
@@ -98,7 +127,9 @@ class _CreateTweetScreenState extends ConsumerState<CreateTweetScreen> {
                 left: 15,
                 right: 15,
               ),
-              child: SvgPicture.asset(AssetsConstants.galleryIcon),
+              child: GestureDetector(
+                onTap: onPickImage,
+                child: SvgPicture.asset(AssetsConstants.galleryIcon)),
             ),
             Padding(
               padding: const EdgeInsets.all(8.0).copyWith(
